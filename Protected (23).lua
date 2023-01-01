a05d3a33e5bc842c31f65aaabac8f0ba0be2d22b
uq6 <100003371+uq6@users.noreply.github.com>
2023-01-01 16:23:59 +0800

    hackerhaha
    HACKED BY N4R#1337

---
+++ Protected (23).lua
@@ -0,0 +1,140 @@
+while wait(1) do game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("im gay", "All") end
+wait(10)
+while wait(1) do game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("im gay", "All") end
+wait(10)
+while wait(1) do game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("im gay", "All") end
+wait(10)
+while wait(1) do game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("im gay", "All") end
+wait(10)
+while wait(1) do game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("im gay", "All") end
+wait(10)
+while wait(1) do game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("im gay", "All") end
+wait(10)
+while wait(1) do game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("im gay", "All") end
+wait(10)
+while wait(1) do game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("im gay", "All") end
+wait(10)
+while wait(1) do game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("im gay", "All") end
+wait(10)
+while wait(1) do game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("im gay", "All") end
+wait(10)
+while wait(1) do game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("im gay", "All") end
+wait(10)
+while wait(1) do game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("im gay", "All") end
+wait(10)
+while wait(1) do game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("im gay", "All") end
+wait(10)
+while wait(1) do game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("im gay", "All") end
+wait(10)
+while wait(1) do game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("im gay", "All") end
+wait(10)
+while wait(1) do game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("im gay", "All") end
+wait(10)
+while wait(1) do game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("im gay", "All") end
+wait(10)
+while wait(1) do game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("im gay", "All") end
+wait(10)
+while wait(1) do game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("im gay", "All") end
+wait(10)
+while wait(1) do game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("im gay", "All") end
+wait(10)
+while wait(1) do game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("im gay", "All") end
+wait(10)
+while wait(1) do game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("im gay", "All") end
+wait(10)
+while wait(1) do game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("im gay", "All") end
+wait(10)
+while wait(1) do game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("im gay", "All") end
+wait(10)
+while wait(1) do game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("im gay", "All") end
+wait(10)
+while wait(1) do game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("im gay", "All") end
+wait(10)
+while wait(1) do game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("im gay", "All") end
+wait(10)
+while wait(1) do game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("im gay", "All") end
+wait(10)
+while wait(1) do game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("im gay", "All") end
+wait(10)
+while wait(1) do game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("im gay", "All") end
+wait(10)
+while wait(1) do game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("im gay", "All") end
+wait(10)
+while wait(1) do game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("im gay", "All") end
+wait(10)
+while wait(1) do game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("im gay", "All") end
+wait(10)
+while wait(1) do game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("im gay", "All") end
+wait(10)
+while wait(1) do game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("im gay", "All") end
+wait(10)
+while wait(1) do game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("im gay", "All") end
+wait(10)
+while wait(1) do game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("im gay", "All") end
+wait(10)
+while wait(1) do game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("im gay", "All") end
+wait(10)
+while wait(1) do game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("im gay", "All") end
+wait(10)
+while wait(1) do game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("im gay", "All") end
+wait(10)
+while wait(1) do game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("im gay", "All") end
+wait(10)
+while wait(1) do game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("im gay", "All") end
+wait(10)
+while wait(1) do game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("im gay", "All") end
+wait(10)
+while wait(1) do game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("im gay", "All") end
+wait(10)
+while wait(1) do game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("im gay", "All") end
+wait(10)
+while wait(1) do game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("im gay", "All") end
+wait(10)
+while wait(1) do game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("im gay", "All") end
+wait(10)
+while wait(1) do game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("im gay", "All") end
+wait(10)
+while wait(1) do game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("im gay", "All") end
+wait(10)
+while wait(1) do game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("im gay", "All") end
+wait(10)
+while wait(1) do game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("im gay", "All") end
+wait(10)
+while wait(1) do game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("im gay", "All") end
+wait(10)
+while wait(1) do game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("im gay", "All") end
+wait(10)
+while wait(1) do game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("im gay", "All") end
+wait(10)
+while wait(1) do game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("im gay", "All") end
+wait(10)
+while wait(1) do game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("im gay", "All") end
+wait(10)
+while wait(1) do game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("im gay", "All") end
+wait(10)
+while wait(1) do game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("im gay", "All") end
+wait(10)
+while wait(1) do game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("im gay", "All") end
+wait(10)
+while wait(1) do game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("im gay", "All") end
+wait(10)
+while wait(1) do game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("im gay", "All") end
+wait(10)
+while wait(1) do game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("im gay", "All") end
+wait(10)
+while wait(1) do game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("im gay", "All") end
+wait(10)
+while wait(1) do game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("im gay", "All") end
+wait(10)
+while wait(1) do game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("im gay", "All") end
+wait(10)
+while wait(1) do game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("im gay", "All") end
+wait(10)
+while wait(1) do game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("im gay", "All") end
+wait(10)
+while wait(1) do game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("im gay", "All") end
+wait(10)
+while wait(1) do game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("im gay", "All") end
+wait(10)
+while wait(1) do game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("im gay", "All") end
+wait(10)
